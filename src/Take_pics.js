a18325d0ee515b7328bf4562137cfcd9aa7c3aff
Axel HR <kevinaxelhr18@gmail.com>
2024-04-22 20:58:44 -0500

    Modifiqué cositas que alfinal no sirvieron pa mucho

--- src/Take_pics.js
+++ src/Take_pics.js
@@ -1,14 +1,12 @@
 var actual_letter = 'A'
 let letters = ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'];
+const full_vid = document.getElementById("full_vid");
+const mini_vid = document.getElementById("mini_vid");
+const canvas = document.getElementById("mini_pic");
+const context = canvas.getContext("2d");
 
 function focus_letter(letter){
     actual_letter = letter
-}
-
-function take_pic(event) {
-    if (event.key == 'Enter'){
-        console.log(actual_letter)
-    }
 }
 
 for (let i = 0; i < letters.length; i++) {
@@ -16,5 +14,51 @@ for (let i = 0; i < letters.length; i++) {
         focus_letter(letters[i]);
     });
 }
+
+const CONSTRAINTS_FULLIMAGE = {
+    video: {weight:{ideal:854}, height:{ideal:480}},
+    audio: false
+}
+const CONSTRAINTS_MINIIMAGE = {
+    video: {width:28, height:28},
+    audio: false
+}
+
+const getVideo = async () => {
+    try{
+        const STREAM_FULL = await navigator.mediaDevices.getUserMedia(CONSTRAINTS_FULLIMAGE);
+        SHOW_full(STREAM_FULL);
+        console.log(STREAM_FULL);
+        
+        const STREAM_MINI = await navigator.mediaDevices.getUserMedia(CONSTRAINTS_MINIIMAGE);
+        SHOW_mini(STREAM_MINI);
+        console.log(STREAM_MINI);
+    }
+    catch (err){
+        console.log(err);
+    }
+};
+
+const SHOW_full = (stream) => {
+    full_vid.srcObject = stream;   
+    full_vid.play();
+};
+
+const SHOW_mini = (stream) => {
+    mini_vid.srcObject = stream;
+    mini_vid.play();
+};
+
+getVideo();
+
+function take_pic(event) {
+    if (event.key === 'Enter') {
+        const selectedVideo = actual_letter === 'A' ? full_vid : mini_vid;
+        context.drawImage(selectedVideo, 0, 0, canvas.width, canvas.height);
+        const imageData = canvas.toDataURL();
+        console.log(imageData);
+    }
+}
+
 
 document.addEventListener('keydown', take_pic);
