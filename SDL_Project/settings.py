fe1b60969fa09c9a34e7ba27ed381db6802173ae
Juan E Ramírez <juanesramirezgalan@gmail.com>
2024-05-04 23:50:40 -0500

    error documentación

--- SDL_Project/settings.py
+++ SDL_Project/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'SDL_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 DATABASES = {
     'default': dj_database_url.config(
         default = 'postgresql://postgres:postgres@localhost/postgres',
